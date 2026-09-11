0c851274b1db1d323710d85b1456d0f785ac043d
hhartz <henri.hartz@gmail.com>
2026-09-11 10:35:19 +0200

    feat: add navbar & init of pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import Collection from "./pages/Collection";
+import History from "./pages/History";
+import Stats from "./pages/Stats";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/stats" element={<Stats />} />
       </Routes>
     </BrowserRouter>
   );
