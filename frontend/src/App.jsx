ad37861539b06c987c4e16545e83af4a08c9aeae
hhartz <henri.hartz@gmail.com>
2026-09-11 11:24:06 +0200

    feat: Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,32 @@ import Collection from "./pages/Collection";
 import History from "./pages/History";
 import Stats from "./pages/Stats";
 
+const styles = {
+  appWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+  },
+  content: {
+    flex: 1,
+    overflowY: "auto",
+  },
+};
+
 export default function App() {
   return (
     <BrowserRouter>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/stats" element={<Stats />} />
-      </Routes>
+      <div style={styles.appWrapper}>
+        <NavBar />
+        <div style={styles.content}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/collection" element={<Collection />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/stats" element={<Stats />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
